d81f00362c5a6073a6a6ca2d8ae3638436341d36
HL <3283152875@qq.com>
2023-11-05 00:11:52 +0800

    🐞 fix: 修复菜单不显示的问题
    https://github.com/ant-design/ant-design-pro/issues/10526#issuecomment-1437769722

--- config/routes.ts
+++ config/routes.ts
@@ -1,16 +1,22 @@
 export default [
-  { path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
-  { path: '/', icon: 'smile', component: './Welcome' },
+  {
+    path: '/user',
+    name: '用户',
+    layout: false,
+    routes: [{ path: '/user/login', name: '登录', component: './User/Login' }],
+  },
+  { path: '/', name: '主页', icon: 'smile', component: './Welcome' },
   {
     path: '/admin',
     icon: 'crown',
+    name: '管理页',
     access: 'canAdmin',
     routes: [
-      { path: '/admin', redirect: '/admin/sub-page' },
-      { path: '/admin/sub-page', component: './Admin' },
+      { path: '/admin', name: '子页', redirect: '/admin/sub-page' },
+      { path: '/admin/sub-page', name: '二级子页', component: './Admin' },
     ],
   },
-  { icon: 'table', path: '/list', component: './TableList' },
+  { icon: 'table', name: '接口管理', path: '/list', component: './TableList' },
   { path: '/', redirect: '/welcome' },
   { path: '*', layout: false, component: './404' },
 ];
